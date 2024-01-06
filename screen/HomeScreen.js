190f3c14da60796049ab79b3180ed1e37316f1bc
Parmilan <parmilancs2024@gmail.com>
2024-01-06 21:55:52 +0530

    some modification

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import { MapPinIcon } from 'react-native-heroicons/solid'
 
 const HomeScreen = () => {
+  const [showSearch, toggleSearch] = useState(false)
+  const [locations, setLocations] = useState([1, 2, 3])
   return (
     <View className='flex-1 relative'>
       {/* Status Bar */}
@@ -16,13 +20,42 @@ const HomeScreen = () => {
       />
       <SafeAreaView className='flex flex-1'>
         <View className='mx-4 my-4 relative z-50'>
-          <View className='flex-row justify-start items-center rounded-3xl bg-primaryGreen'>
-            <TextInput
-              placeholder='Search City'
-              placeholderTextColor={'lightgray'}
-              className='pl-5 h-10 flex-1 text-base text-white'
-            />
+          <View
+            className={`flex-row justify-end items-center rounded-full ${
+              showSearch ? 'bg-primaryGreen' : 'transparent'
+            }`}
+          >
+            {/* Search Field */}
+            {showSearch ? (
+              <TextInput
+                placeholder='Search City'
+                placeholderTextColor={'lightgray'}
+                className='pl-5 h-10 flex-1 text-base text-white'
+              />
+            ) : null}
+            {/* Search Button */}
+            <TouchableOpacity
+              onPress={() => toggleSearch(!showSearch)}
+              className='m-1 p-3 rounded-full bg-slate-400 transp'
+            >
+              <MagnifyingGlassIcon size={25} color='white' />
+            </TouchableOpacity>
           </View>
+          {locations.length > 0 && showSearch ? (
+            <View className=' absolute w-full bg-gray-300 rounded-3xl top-16'>
+              {locations.map((location, i) => {
+                return (
+                  <TouchableOpacity
+                    key={i}
+                    className='flex-row items-center px-4 py-3 mx-1'
+                  >
+                    <MapPinIcon size={25} color='gray' />
+                    <Text className='text-base'>Kilinochchi, Sri Lanka</Text>
+                  </TouchableOpacity>
+                )
+              })}
+            </View>
+          ) : null}
         </View>
       </SafeAreaView>
     </View>
